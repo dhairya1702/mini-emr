9574cf6186c84ca7ebe9e37ba30c72aa7ef51bbb
Dhairya Lalwani <dhairya.lalwani2001@gmail.com>
2026-07-15 18:08:10 +0530

    Escape super admin allowlist during deploy

--- scripts/deploy-backend.sh
+++ scripts/deploy-backend.sh
@@ -58,7 +58,7 @@ gcloud run deploy "$BACKEND_SERVICE" \
   --set-env-vars="GEMINI_MODEL=gemini-3.5-flash" \
   --set-env-vars="APP_ORIGIN=${WEB_URL}" \
   --set-env-vars="^@^APP_ORIGINS=${WEB_ORIGINS}" \
-  --set-env-vars="SUPER_ADMIN_IDENTIFIERS=${SUPER_ADMIN_IDENTIFIERS}" \
+  --set-env-vars="^|^SUPER_ADMIN_IDENTIFIERS=${SUPER_ADMIN_IDENTIFIERS}" \
   --set-env-vars="OPEN_CLINIC_REGISTRATION=true" \
   --set-env-vars="FOLLOW_UP_REMINDER_RUNNER_ENABLED=false" \
   --set-env-vars="FOLLOW_UP_REMINDER_INTERVAL_SECONDS=300" \
